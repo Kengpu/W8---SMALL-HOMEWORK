0fbf1f8fb3caa3ae0b0bcb8537a7c423081f8192
Keng Puthearith <puthearithkeng7@gmail.com>
2026-03-11 22:33:37 +0700

    update

--- lib/data/repositories/songs/song_repository_mock.dart
+++ lib/data/repositories/songs/song_repository_mock.dart
@@ -47,11 +47,14 @@ class SongRepositoryMock implements SongRepository {
   @override
   Future<Song?> fetchSongById(String id) async {
     // - Simulate a delay of 3 seconds.
-
+     await Future.delayed(const Duration(seconds: 3));
     // - After the delay : Find the song of given id in the list of songs and return it
-
+        try {
+      final song = _songs.firstWhere((song) => song.id == id);
+      return song;
+    } catch (e){
+      throw Exception("no song found for id $id in the data");
+    }
     // - If not found : Throw an error with the message “no song found for id 25 in the database"
-
-    return Future.delayed(Duration.zero); // TO CHANGE !
   }
 }
